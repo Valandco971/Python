035e5e3f1e57c6a20ecf8f1f4f4c40c4e991655d
Valandco971 <valandco971@gmail.com>
2017-10-28 19:44:54 +0000

    Maj_exercice_4.7.py

--- exercice_4.7.py
+++ exercice_4.7.py
@@ -1,2 +1,21 @@
 # Programme qui affiche les 20 premiers termes de la table de multiplication par # 7, en signalant au passage (à l'aide d'une astérisque) ceux qui sont des       # multiples de 3.
 
+i, j = 0, 7
+
+while(i<20):
+
+    if((i*j)%3!=0):
+
+        print(i*j,end=" ")
+
+    elif((i*j)%3==0):
+
+        print(i*j,"*",end="")
+
+    i = i+1
+
+
+
+
+
+
