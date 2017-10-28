46d7fd8474067f4a0dfdbfc53059d252d894f6cb
Valandco971 <valandco971@gmail.com>
2017-10-28 11:25:13 +0000

    ajout d'un fichier

---
+++ exercice_4.5.py
@@ -0,0 +1,8 @@
+# Petit programme qui calcule le volume d'un parallélépipède rectangle dont
+# son fournis au départ la largeur, la hauteur et la profondeur.
+
+largeur, longueur, profondeur = 100, 200, 50
+
+volume = largeur* longueur* profondeur
+
+print("le volume total est donc:", volume)
